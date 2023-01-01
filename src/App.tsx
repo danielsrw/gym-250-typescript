04586518d8854ff4af118a89fe6cd650c4a3c255
Andela Rwanda <danielsrwanda@gmail.com>
2023-01-01 15:48:48 +0200

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Navbar from "@/scenes/navbar";
-import Home from "@/scenes/home";
-import OurClasses from "@/scenes/ourClasses";
-import Benefits from "@/scenes/benefits";
-import ContactUs from "@/scenes/contactUs";
-import Footer from "@/scenes/footer";
+import Navbar from "./scenes/navbar";
+import Home from "./scenes/home";
+import OurClasses from "./scenes/ourClasses";
+import Benefits from "./scenes/benefits";
+import ContactUs from "./scenes/contactUs";
+import Footer from "./scenes/footer";
 import { useEffect, useState } from "react";
-import { SelectedPage } from "@/shared/types";
+import { SelectedPage } from "./shared/types";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
